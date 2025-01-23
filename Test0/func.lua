d8092552061ed05e1847326284016af8e7112f15
Tianyao Zhan <879347657@qq.com>
2025-01-23 10:20:29 +0800

    V0.1.2 Love2D开发入门
    练习一
    实现了内容物运动逻辑，包括其自身大小变化、框体与内容物碰撞等

--- Test0/func.lua
+++ Test0/func.lua
@@ -1,23 +1,24 @@
 --根据滑块运动方向更新滑块位置的函数
-function blockMove(rec_x1,rec_y1,MoveState,dt)
+function blockMove(rec_x,rec_y,MoveState,dt)
     local state=MoveState
+    speed=100
     local switch=
     {
         [MoveStates[1]] = function ()
-            rec_x1=rec_x1+15*dt
-            rec_y1=rec_y1+15*dt
+            rec_x=rec_x+speed*dt
+            rec_y=rec_y+speed*dt
         end,
         [MoveStates[2]] = function ()
-            rec_x1=rec_x1+15*dt
-            rec_y1=rec_y1-15*dt
+            rec_x=rec_x+speed*dt
+            rec_y=rec_y-speed*dt
         end,
         [MoveStates[3]] = function ()
-            rec_x1=rec_x1-15*dt
-            rec_y1=rec_y1-15*dt
+            rec_x=rec_x-speed*dt
+            rec_y=rec_y-speed*dt
         end,
         [MoveStates[4]] = function ()
-            rec_x1=rec_x1-15*dt
-            rec_y1=rec_y1+15*dt
+            rec_x=rec_x-speed*dt
+            rec_y=rec_y+speed*dt
         end
     }
     
@@ -26,13 +27,67 @@ function blockMove(rec_x1,rec_y1,MoveState,dt)
         moveFunc()
     else
     end
-    return rec_x1,rec_y1
+    return rec_x,rec_y
 end
 --控制滑块大小的函数
-function blockSize(rec_x1,rec_y1,dt)
-    rec_x2=rec_x1+20*dt
-    rec_y2=rec_y1+20*dt
-    return rec_x2,rec_y2
+function blockSize()
+    rec_width=25*(1+math.sin(sizeCtrl)/10)
+    rec_length=25*(1+math.sin(sizeCtrl)/10)
+    return rec_width,rec_length
 end
 --判断滑块是否触碰边框的函数
+function isColliding(rec_x,rec_y,rec_width,rec_length)
+    if rec_x<=outline_x then
+        return sides[3]
+    elseif rec_x+rec_width>=outline_x+outline_width then
+        return sides[4]
+    elseif rec_y<=outline_y then
+        return sides[1]
+    elseif rec_y+rec_length>=outline_y+outline_length then
+        return sides[2]
+    else
+        return sides[5]
+    end
+end
 --控制滑块运动方向的函数
+function changeMoveState(side,MoveState)
+    local switch=
+    {
+        [sides[1]] = function ()
+            if MoveState==MoveStates[2] then
+                MoveState=MoveStates[1]
+            elseif MoveState==MoveStates[3] then
+                MoveState=MoveStates[4]
+            end
+        end,
+        [sides[2]] = function ()
+            if MoveState==MoveStates[1] then
+                MoveState=MoveStates[2]
+            elseif MoveState==MoveStates[4] then
+                MoveState=MoveStates[3]
+            end
+        end,
+        [sides[3]] = function ()
+            if MoveState==MoveStates[4] then
+                MoveState=MoveStates[1]
+            elseif MoveState==MoveStates[3] then
+                MoveState=MoveStates[2]
+            end
+        end,
+        [sides[4]] = function ()
+            if MoveState==MoveStates[1] then
+                MoveState=MoveStates[4]
+            elseif MoveState==MoveStates[2] then
+                MoveState=MoveStates[3]
+            end
+        end,
+        [sides[5]] = function ()
+        end
+    }
+    local changeFunc=switch[side]
+    if changeFunc then
+        changeFunc()
+    else
+    end
+    return MoveState
+end
--- Test0/main.lua
+++ Test0/main.lua
@@ -3,34 +3,43 @@ require("func")
 
 --初始化
 function love.load()
-    --边框大小
-    outline_x1=20
-    outline_y1=20
-    outline_x2=520
-    outline_y2=320
+    --边框位置、大小
+    outline_x=20
+    outline_y=20
+    outline_width=500
+    outline_length=300
+    --滑块位置、大小
+    rec_x=20
+    rec_y=20
+    rec_width=20
+    rec_length=20
+    sizeCtrl=1 
     --滑块移动方向，初始方向为向右下移动
     MoveStates={"RightDown","RightUp","LeftUp","LeftDown"}
     MoveState=MoveStates[1]
-    --滑块大小
-    rec_x1=20
-    rec_y1=20
-    rec_x2=rec_x1+20
-    rec_y2=rec_y1+20
+    --滑块与边框相撞时所处位置（不接触时为“NONE”）
+    sides={"TOP","BOTTOM","LEFT","RIGHT","NONE"}
+    side=sides[5]
 end
 
 --循环更新
 function love.update(dt)
+    --判断滑块是否与边框相撞
+    side=isColliding(rec_x,rec_y,rec_width,rec_length)
+    --根据滑块与边框相撞的位置更新滑块运动方向
+    MoveState=changeMoveState(side,MoveState)
     --更新滑块位置
-    rec_x1,rec_y1=blockMove(rec_x1,rec_y1,MoveState,dt)
+    rec_x,rec_y=blockMove(rec_x,rec_y,MoveState,dt)
     --更新滑块大小
-    rec_x2,rec_y2=blockSize(rec_x1,rec_y1,dt)
+    sizeCtrl=sizeCtrl+12*dt --提供一个时间变量以供产生尺寸控制函数中正弦量的变化，但是这个值可能会溢出导致崩溃
+    rec_width,rec_length=blockSize()
     
 end
 
 --循环绘制
 function love.draw()
     --绘制边框
-    love.graphics.rectangle("line",outline_x1,outline_y1,outline_x2,outline_y2)
+    love.graphics.rectangle("line",outline_x,outline_y,outline_width,outline_length)
     --绘制滑块
-    love.graphics.rectangle("fill",rec_x1,rec_y1,rec_x2,rec_y2)
+    love.graphics.rectangle("fill",rec_x,rec_y,rec_width,rec_length)
 end
